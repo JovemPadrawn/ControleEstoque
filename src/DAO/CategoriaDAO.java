cd4a8bddd97e7004ea00d4423ba2196a262ffb54
JovemPadrawn <niko-silva95@hotmail.com>
2025-11-11 21:21:29 -0300

    codando a tabela categoria no form e integração com o bdd

---
+++ src/DAO/CategoriaDAO.java
@@ -0,0 +1,75 @@
+package DAO;
+
+import Modelo.Categoria;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoriaDAO {
+    public boolean inserir(Categoria categoria){
+        String sql = "INSERT INTO categorias (nome_categoria) VALUES (?)";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, categoria.getNome_categoria());
+            stmt.executeUpdate();
+            return true;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Categoria> listarTodos(){
+        List<Categoria> categorias = new ArrayList<>();
+        String sql = "SELECT * FROM categorias order by nome_categoria ASC";
+
+        try(
+                Connection conn = Conexao.conectar();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()
+        ){
+            while(rs.next()){
+                Categoria p = new Categoria();
+                p.setId_categoria(rs.getInt("id_categoria"));
+                p.setNome_categoria(rs.getString("nome_categoria"));
+                categorias.add(p);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return categorias;
+    }
+
+    public boolean atualizar(Categoria produto){
+        String sql = "UPDATE categorias SET nome_categoria = ? WHERE id_categoria = ?";
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, produto.getNome_categoria());
+            stmt.setInt(2, produto.getId_categoria());
+            stmt.executeUpdate();
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean excluir(int id){
+        String sql = "DELETE FROM categorias WHERE id_categoria = ?";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -6,81 +6,41 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ProdutoDAO produtoDAO = new ProdutoDAO();
+        FormGerenciadorEstoque form = new FormGerenciadorEstoque();
+        form.setVisible(true);
 
-        Scanner scanopcao = new Scanner(System.in);
-
+        /*ProdutoDAO produtoDAO = new ProdutoDAO();
+        Scanner scan = new Scanner(System.in);
         int opcao;
-        do {
-            System.out.println("----------------------------------------");
-            System.out.println("------------------MENU------------------");
-            System.out.println("----------------------------------------");
-            System.out.println("1-inserir produtos----------------------");
-            System.out.println("2-atualizar produtos--------------------");
-            System.out.println("3-excluir produtos----------------------");
-            System.out.println("4-exibir produtos-----------------------");
-            System.out.println("5-sair----------------------------------");
-            System.out.println("----------------------------------------");
-            System.out.println("opção: ");
-            opcao =scanopcao.nextInt();
+
+        do{
+            System.out.println("**** SISTEMA DE CONTROLE DE ESTOQUE ****");
+            System.out.println("1 - Cadastrar Produto");
+            System.out.println("2 - Listar");
+            System.out.println("3 - Atualizar Produto");
+            System.out.println("4 - Excluir");
+            System.out.println("6 - Sair do Sistema");
+            System.out.println("Escolha uma opção no menu: ");
+            opcao = scan.nextInt();
 
             switch (opcao){
                 case 1:
+                    //Inserir Produto
                     Produto produto = new Produto();
-                    System.out.println("Informe os dados do produto");
-                    scanopcao.nextLine();
+
+                    System.out.println("Informe os dados do produto:");
+                    scan.nextLine();//limpa buffer
                     System.out.println("Nome: ");
-                    produto.setNome(scanopcao.nextLine());
+                    produto.setNome(scan.nextLine());
                     System.out.println("Quantidade: ");
-                    produto.setQuantidade(scanopcao.nextInt());
+                    produto.setQuantidade(scan.nextInt());
                     System.out.println("Preço: ");
-                    produto.setPreco(scanopcao.nextDouble());
+                    produto.setPreco(scan.nextDouble());
+
                     produtoDAO.inserir(produto);
                     break;
                 case 2:
-                    System.out.println("Informe o ID do produto: ");
-                    int codigo = scanopcao.nextInt();
-
-                    /*
-                    fazer a validação da existencia do produto
-                    chamar um metodo buscarID(id)
-                    segunda versao
-                    retornaria todos os dados do objeto para nao termos valores nulos
-                    */
-
-
-                    Produto produtoAtualizar = new Produto();
-
-                    System.out.println("Informe os novos dados do produto");
-                    scanopcao.nextLine();
-                    System.out.println("Nome: ");
-                    produtoAtualizar.setNome(scanopcao.nextLine());
-                    System.out.println("Quantidade: ");
-                    produtoAtualizar.setQuantidade(scanopcao.nextInt());
-                    System.out.println("Preço: ");
-                    produtoAtualizar.setPreco(scanopcao.nextDouble());
-
-                    produtoAtualizar.setId(codigo);
-                    produtoDAO.atualizar(produtoAtualizar);
-                    break;
-                case 3:
-                    System.out.println("Escolha o ID do produto a qual quer excluir");
-                    System.out.println("ID: ");
-                    int codigoExcluir = scanopcao.nextInt();
-                    scanopcao.nextLine();
-                    System.out.println("Voce realmente deseja exluir este produto?");
-                    System.out.println("Sim ou não(s - n): ");
-                    String confirmar = scanopcao.nextLine();
-                    if (confirmar.equals("s")){
-                        produtoDAO.excluir(codigoExcluir);
-                    } else if (confirmar.equals("n")){
-                        System.out.println("Ação cancelada");
-                    } else {
-                        System.out.println("Erro Fatal uau!");
-                    }
-                    break;
-                case 4:
-                    for (Produto p : produtoDAO.listarTodos()) {
+                    for(Produto p : produtoDAO.listarTodos()) {
                         System.out.print("\n*****************************");
                         System.out.print("\nID: " + p.getId());
                         System.out.print("\nProduto: " + p.getNome());
@@ -89,23 +49,69 @@ public class Main {
                         System.out.print("\nTotal R$ " + p.getQuantidade() * p.getPreco());
                     }
                     break;
-                case 5:
-                    System.out.println("saindo...");
+                case 3:
+                    //Atualizar Produto
+                    System.out.println("Informe o código do produto:");
+                    int codigo = scan.nextInt();
+
+                    //fazer a validação da existencia deste produto pelo metodo buscarId(id)
+                    //Segunda versão
+                    //retornar todos os dados do objeto para não termos valores nulos
+
+                    Produto produtoAtualizar = new Produto();
+
+                    System.out.println("Informe os NOVOS dados do produto:");
+                    scan.nextLine();//limpa buffer
+                    System.out.println("Nome: ");
+                    produtoAtualizar.setNome(scan.nextLine());
+                    System.out.println("Quantidade: ");
+                    produtoAtualizar.setQuantidade(scan.nextInt());
+                    System.out.println("Preço: ");
+                    produtoAtualizar.setPreco(scan.nextDouble());
+
+                    produtoAtualizar.setId(codigo);
+                    produtoDAO.atualizar(produtoAtualizar);
                     break;
-                default:
-                    System.out.println("Erro fatal!!");
+                case 4:
+                    //Excluir Produto
+                    System.out.println("Informe o código do produto:");
+                    int codigoExluir = scan.nextInt();
+
+                    produtoDAO.excluir(codigoExluir);
+
                     break;
             }
-        }while (opcao !=5);
+        }while(opcao != 6);
+
+        /*
+        ATIVIDADES A SEREM REALIZADAS
+        TERMINAL
+        1 - Implementar o menu - ok
+        2 - Implementar os cases para o CRUD - ok
+        3 - Implementar as entradas de valores para o insert - ok
+        4 - Solicitar o código, listar o produto, e implementar as entradas de valores para o update
+        5 - Solicitar o código, e perguntar se realmente deseja excluir o produtom, se sim excluir
+        6 - Atualizar o excluir para ativar e inativar
+
+        SWING
+        1 - Implementar o menu
+        2 - Implementar os BOTÕES E O JTABLE para o CRUD
+        3 - Implementar as entradas de valores para o insert
+        4 - Solicitar o código, listar o produto, e implementar as entradas de valores para o update
+        5 - Solicitar o código, e perguntar se realmente deseja excluir o produtom, se sim excluir
+        6 - Atualizar o excluir para ativar e inativar
+        */
+
+        /*
+
+        */
+        /*
+
+        */
+        /*
+
+        */
 
 
-        //Produto produto = new Produto("Arroz ABE 5 kg", 2, 35.98);
-        //produtoDAO.inserir(produto);
-
-        //Produto produtoAtualizar = new Produto("Leite Integral", 12, 3.98);
-        //produtoAtualizar.setId(1);
-        //produtoDAO.atualizar(produtoAtualizar);
-
-       //produtoDAO.excluir(4);
     }
 }
